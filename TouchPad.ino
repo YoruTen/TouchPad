12a6fb4d6791ff832cfd6f8fbfa6137c933f1677
YoruTen <andreas.gustavsson92@gmail.com>
2021-09-21 13:36:04 +0200

    Update TouchPad.ino

--- TouchPad.ino
+++ TouchPad.ino
@@ -1,8 +1,8 @@
 #include <Synaptics.h>
 #include <Mouse.h>
 
-#define DATAPIN 5 
-#define CLKPIN  6 
+#define DATAPIN 5
+#define CLKPIN 6
 
 Synaptics pad(CLKPIN, DATAPIN);
 
@@ -16,81 +16,56 @@ const int sensitivity = 1;
 int responseDelay = 2;
 int start = 1;
 
-void pad_init()
-{
+void pad_init(){ // Initiate the touchpad
   info = pad.reset();
   Serial.print("Reset status: ");
   Serial.println(info);
-  pad.set_remote_mode();  // remote mode
-  //Serial.println("Hello PAD2");
-  //pad.read();  // ack
-  //Serial.println("Hello PAD3");
+  pad.set_remote_mode(); // remote mode
   delayMicroseconds(100);
-  //info = pad.isRelative();
-  //Serial.println(info);
-  //mode = pad.read_modes();
-  //Serial.println(mode); 
-  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("SETUP");
   pad_init();
-  Mouse.begin();
-  }
+  Mouse.begin(); // Start mouse HID protocol
+}
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if (start < 10) {
+void loop() { // put your main code here, to run repeatedly:
+  if (start < 10) { // Prints loop 10 times to show it has started
     Serial.println("LOOP");
     start = start+1;
   }
-  pad.read_data();
-//  pos = pad.read();
-//  Serial.println(pos);
-  
-//  y=pad.getY();
-//  x=pad.getX();
-//  l=pad.leftClicked();
-//  r=pad.rightClicked();
-//  Serial.print(x); // X Movement Data
-//  Serial.print(",");
-//  Serial.print(y);
-//  Serial.print(":");
-//  Serial.print(l);
-//  Serial.print(",");
-//  Serial.print(r);
+  pad.read_data(); // Tell pad to read and store data
+  vertValue = -(pad.getY()); // read vertical offset
+  horzValue = pad.getX(); // read horizontal offset
 
-  vertValue = -(pad.getY());  // read vertical offset
-  horzValue = pad.getX();  // read horizontal offset
-//  Serial.print(":");
-  if(vertValue != 0 || horzValue != 0){
+  if(vertValue != 0 || horzValue != 0)  { // Print movement
     Serial.print(vertValue); // X Movement Data
     Serial.print(",");
     Serial.print(horzValue);
     Serial.println();
   }
-  Mouse.move(horzValue, vertValue, 0);
-
+  Mouse.move(horzValue, vertValue, 0); // Move mouse cursor
   delay(responseDelay);
-  if (pad.leftClicked()==1)  {
-    if  (!Mouse.isPressed(MOUSE_LEFT)){
+
+  if (pad.leftClicked()==1) { // Check if left button is clicked
+    if (!Mouse.isPressed(MOUSE_LEFT)){
       Mouse.press(MOUSE_LEFT);
     }
   }
-  else  {
-    if  (Mouse.isPressed(MOUSE_LEFT)){
+  else {
+    if (Mouse.isPressed(MOUSE_LEFT)){ //release if no longer pressed
       Mouse.release(MOUSE_LEFT);
     }
-  }
-  if (pad.rightClicked()==1)  {
-    if  (!Mouse.isPressed(MOUSE_RIGHT)){
+   }
+  if (pad.rightClicked()==1) { // Check if right button is clicked
+    if (!Mouse.isPressed(MOUSE_RIGHT)){
       Mouse.press(MOUSE_RIGHT);
     }
   }
-  else  {
-    if  (Mouse.isPressed(MOUSE_RIGHT)){
+  else {
+    if (Mouse.isPressed(MOUSE_RIGHT)){ //release if no longer pressed
       Mouse.release(MOUSE_RIGHT);
     }
   }
